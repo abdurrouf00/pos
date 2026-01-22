4177ae5c39cc745871e40ec7a748a8827d1604d0
abdur rouf <abdurroufkpkh@gmail.com>
2026-01-22 16:19:53 +0600

    e

--- next.config.js
+++ next.config.js
@@ -1,13 +1,37 @@
+// /** @type {import('next').NextConfig} */
+// const path = require('path');
+
+// const nextConfig = {
+//   /* config options here */
+//   webpack: (config) => {
+//     // Add path alias for '@'
+//     config.resolve.alias['@'] = path.join(__dirname, 'src');
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig; 
+
+
+
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
 const nextConfig = {
-  /* config options here */
-  webpack: (config) => {
-    // Add path alias for '@'
+  reactStrictMode: true,
+  turbopack: {}, // suppress warning
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config, { isServer }) => {
+    // optional: keep webpack alias if building with webpack
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
   },
+  // Turbopack native alias for `@`
+  experimental: {
+    externalDir: true,
+  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
