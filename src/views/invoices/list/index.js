173c8733b685ac8b32b013044177e84098cce702
abdur rouf <abdurroufkpkh@gmail.com>
2026-02-01 15:35:19 +0600

    update

--- src/views/invoices/list/index.js
+++ src/views/invoices/list/index.js
@@ -15,7 +15,7 @@ export default function SecondHeader() {
   return (
     <div className="border border-gray-100 bg-white  px-4 pr-10 rounded">
       <div className="flex items-center justify-between h-10 md:h-12  ">
-        <div className="   hover:bg-gray-100 font-bold text-2xl p-2 rounded">All {title}s</div>
+        <div className="   hover:bg-gray-100 font-bold text-2xl p-2 rounded">All {title}</div>
           <>
           <Link href="/dashboard/invoices/new-invoice">
               <Button>
